a00124054ba96b2ce6a9f46c54b7e4d2a40c4dd9
adrianlukito <lukito.adrian1@gmail.com>
2021-08-30 21:40:26 +0700

    Accessing Activity Instance

--- app/src/androidTest/java/com/codingwithmitch/espressodaggerexamples/ui/ListFragmentIntegrationTests.kt
+++ app/src/androidTest/java/com/codingwithmitch/espressodaggerexamples/ui/ListFragmentIntegrationTests.kt
@@ -1,7 +1,10 @@
 package com.codingwithmitch.espressodaggerexamples.ui
 
+import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.Observer
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
@@ -10,6 +13,9 @@ import com.codingwithmitch.espressodaggerexamples.R
 import com.codingwithmitch.espressodaggerexamples.TestBaseApplication
 import com.codingwithmitch.espressodaggerexamples.di.TestAppComponent
 import com.codingwithmitch.espressodaggerexamples.util.Constants
+import com.codingwithmitch.espressodaggerexamples.util.Constants.BLOG_POSTS_DATA_FILENAME
+import com.codingwithmitch.espressodaggerexamples.util.Constants.CATEGORIES_DATA_FILENAME
+import com.codingwithmitch.espressodaggerexamples.util.Constants.EMPTY_LIST
 import com.codingwithmitch.espressodaggerexamples.util.EspressoIdlingResourceRule
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.InternalCoroutinesApi
@@ -42,8 +48,8 @@ class ListFragmentIntegrationTests: BaseMainActivityTests() {
             .applicationContext as TestBaseApplication
 
         val apiService = configureFakeApiService(
-            blogsDataSource = Constants.EMPTY_LIST, // empty list of data
-            categoriesDataSource = Constants.CATEGORIES_DATA_FILENAME,
+            blogsDataSource = EMPTY_LIST, // empty list of data
+            categoriesDataSource = CATEGORIES_DATA_FILENAME,
             networkDelay = 0L,
             application = app
         )
@@ -61,6 +67,43 @@ class ListFragmentIntegrationTests: BaseMainActivityTests() {
 
         onView(withId(R.id.no_data_textview))
             .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+    }
+
+    @Test
+    fun isCategoryListEmpty() {
+
+        // setup
+        val app = InstrumentationRegistry
+            .getInstrumentation()
+            .targetContext
+            .applicationContext as TestBaseApplication
+
+        val apiService = configureFakeApiService(
+            blogsDataSource = BLOG_POSTS_DATA_FILENAME, // empty list of data
+            categoriesDataSource = EMPTY_LIST,
+            networkDelay = 0L,
+            application = app
+        )
+
+        configureFakeRepository(apiService, app)
+
+        injectTest(app)
+
+        val scenario = launchActivity<MainActivity>().onActivity { mainActivity ->
+            val toolbar: Toolbar = mainActivity.findViewById(R.id.tool_bar)
+
+            mainActivity.viewModel.viewState.observe(mainActivity, Observer { viewState ->
+                if(viewState.activeJobCounter.size == 0) {
+                    toolbar.showOverflowMenu()
+                }
+            })
+        }
+
+        // assert
+        onView(withSubstring("earthporn")).check(doesNotExist())
+        onView(withSubstring("dogs")).check(doesNotExist())
+        onView(withSubstring("fun")).check(doesNotExist())
+        onView(withSubstring("All")).check(matches(isDisplayed()))
     }
 
     override fun injectTest(application: TestBaseApplication) {
