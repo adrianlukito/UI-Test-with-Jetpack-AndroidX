a6e1a7bd0190f879e5e216f47c2f5fe94e78ad31
adrianlukito <lukito.adrian1@gmail.com>
2021-08-30 19:22:22 +0700

    BaseTesting Class

--- app/src/androidTest/java/com/codingwithmitch/espressodaggerexamples/ui/DetailFragmentTest.kt
+++ app/src/androidTest/java/com/codingwithmitch/espressodaggerexamples/ui/DetailFragmentTest.kt
@@ -9,11 +9,9 @@ import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.platform.app.InstrumentationRegistry
 import com.codingwithmitch.espressodaggerexamples.R
 import com.codingwithmitch.espressodaggerexamples.TestBaseApplication
-import com.codingwithmitch.espressodaggerexamples.api.FakeApiService
 import com.codingwithmitch.espressodaggerexamples.di.TestAppComponent
 import com.codingwithmitch.espressodaggerexamples.fragments.FakeMainFragmentFactory
 import com.codingwithmitch.espressodaggerexamples.models.BlogPost
-import com.codingwithmitch.espressodaggerexamples.repository.FakeMainRespositoryImpl
 import com.codingwithmitch.espressodaggerexamples.ui.viewmodel.setSelectedBlogPost
 import com.codingwithmitch.espressodaggerexamples.util.Constants.BLOG_POSTS_DATA_FILENAME
 import com.codingwithmitch.espressodaggerexamples.util.Constants.CATEGORIES_DATA_FILENAME
@@ -28,7 +26,6 @@ import io.mockk.mockk
 import io.mockk.runs
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.InternalCoroutinesApi
-import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -37,7 +34,7 @@ import javax.inject.Inject
 @ExperimentalCoroutinesApi
 @InternalCoroutinesApi
 @RunWith(AndroidJUnit4ClassRunner::class)
-class DetailFragmentTest {
+class DetailFragmentTest: BaseMainActivityTests() {
 
     @Inject
     lateinit var viewModelFactory: FakeMainViewModelFactory
@@ -105,31 +102,7 @@ class DetailFragmentTest {
             .check(matches(withText(selectedBlogPost.body)))
     }
 
-    fun injectTest(application: TestBaseApplication) {
+    override fun injectTest(application: TestBaseApplication) {
         (application.appComponent as TestAppComponent).inject(this)
-    }
-
-    fun configureFakeApiService(
-        blogDataSource: String? = null,
-        categoriesDataSource: String? = null,
-        networkDelay: Long? = null,
-        application: TestBaseApplication
-    ): FakeApiService {
-        val apiService = (application.appComponent as TestAppComponent).apiService
-        blogDataSource?.let { apiService.blogPostsJsonFileName = it }
-        categoriesDataSource?.let { apiService.categoriesJsonFileName = it }
-        networkDelay?.let { apiService.networkDelay = it }
-
-        return apiService
-    }
-
-    fun configureFakeRepository(
-        apiService: FakeApiService,
-        application: TestBaseApplication
-    ): FakeMainRespositoryImpl {
-        val mainRepository = (application.appComponent as TestAppComponent).mainRepository
-        mainRepository.apiService = apiService
-
-        return mainRepository
     }
 }
